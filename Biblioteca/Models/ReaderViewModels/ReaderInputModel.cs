008dd855b9b6ee9d888b76696f5951bd2e00a45f
MihailCorcimari <mhcorcimari@gmail.com>
2025-11-07 19:25:09 +0000

    Store reader profile images in the database

--- Biblioteca/Models/ReaderViewModels/ReaderInputModel.cs
+++ Biblioteca/Models/ReaderViewModels/ReaderInputModel.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace Biblioteca.Models.ReaderViewModels
 {
@@ -24,9 +25,14 @@ namespace Biblioteca.Models.ReaderViewModels
         [DataType(DataType.Date)]
         public DateTime? BirthDate { get; set; }
 
-        [Display(Name = "Foto de perfil (URL)")]
-        [StringLength(512)]
-        public string? ProfileImageUrl { get; set; }
+        [Display(Name = "Foto de perfil")]
+        public IFormFile? ProfileImageFile { get; set; }
+
+        [Display(Name = "Foto de perfil")]
+        public string? ProfileImageDataUrl { get; set; }
+
+        [Display(Name = "Remover foto de perfil")]
+        public bool RemoveProfileImage { get; set; }
 
         [Display(Name = "Código de leitor")]
         public string ReaderCode { get; set; } = string.Empty;
